c71882ed2039c197c6119d31362d6e12b7a71dee
raffithoriq <thoriqibb@gmail.com>
2023-09-27 17:01:14 +0800

    TugasPBO

---
+++ Bengkelmotor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.posttest1;
+
+/**
+ *
+ * @author username
+ */
+public class Bengkelmotor {
+    
+}
---
+++ Posttest1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.posttest1;
+
+/**
+ *
+ * @author username
+ */
+public class Posttest1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
