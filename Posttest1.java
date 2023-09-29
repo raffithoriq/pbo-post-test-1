870532b802b606e4457df606f9b41802e37eeed7
raffithoriq <126893861+raffithoriq@users.noreply.github.com>
2023-09-30 01:37:44 +0800

    Posttest1.java

--- Posttest1.java
+++ Posttest1.java
@@ -2,15 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.posttest1;
+package project.posttest1;
 
 /**
  *
  * @author username
  */
-public class Posttest1 {
 
+public class Posttest1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        DealerMobil dealer = new DealerMobil("Dealer ABC");
+
+        dealer.tambahMobil("Nissan Sentra");
+        dealer.tambahMobil("Chevrolet Cruze");
+        dealer.tambahMobil("Hyundai Elantra");
+        dealer.tambahMobil("Toyota Supra MK14");
+        dealer.tambahMobil("Honda Brio");
+        
+        dealer.tampilkanDaftarMobil();
     }
 }
